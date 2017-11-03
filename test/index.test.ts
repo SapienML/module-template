ffbd5650f163a4f05cfef9c74adbe7e6bd75097c
junaid <junaid1460@gmail.com>
2017-11-03 22:50:56 +0530

    Removed test code since template has nothing to test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,8 @@
 import { suite, test, slow, timeout } from "mocha-typescript";
 import * as assert from "assert" ;
-import { Tensor } from '@types';
 // import { hello } from '../src/index';
 @suite class Test{
     @test testHello(){
-        var t = new Tensor()
+       
     }
 }
